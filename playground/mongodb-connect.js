b2ce25f51698922e990ca95a228a2b6f45a7f2e2
Hemant Achhami <hemantachhami19@gmail.com>
2018-11-11 09:36:22 +0545

    Fetching all todos

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -6,31 +6,40 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log("Connecting with the mongo server");
     const db = client.db('TodoApp');
 
-     // Insert one
-    db.collection('TodoApp').insertOne({
-        text: 'test text1',
-        completed: false,
-    }, (error, result) => {
-        if (error) {
-            return console.log("error");
-        }
-        console.log(JSON.stringify(result.ops));
+    //  // Insert one
+    // db.collection('TodoApp').insertOne({
+    //     text: 'test text1',
+    //     completed: false,
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log("error");
+    //     }
+    //     console.log(JSON.stringify(result.ops));
+    // });
+    //
+    // //InsertMany object
+    // todoObjectList = [
+    //     {
+    //         text:'Read a book',
+    //         completed:true
+    //     },
+    //     {
+    //         text:'complete node js crud',
+    //         completed:false
+    //     }
+    // ];
+    // db.collection("todos").insertMany(todoObjectList,(error,result)=>{
+    //     if(error) throw  error;
+    //     console.log(result.ops);
+    // });
+
+    //Fetch all item:
+    db.collection('todos').find({}).toArray((error,result)=>{
+        if (error) throw  error;
+        console.log(result);
     });
 
-    //InsertMany object
-    todoObjectList = [
-        {
-            text:'Read a book',
-            completed:true
-        },
-        {
-            text:'complete node js crud',
-            completed:false
-        }
-    ];
-    db.collection("todos").insertMany(todoObjectList,(error,result)=>{
-        if(error) throw  error;
-        console.log(result.ops);
-    });
+
+
     client.close();
 });
