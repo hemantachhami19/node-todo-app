1e348ca53d618a0a0a02a9e1003efb46f4586f77
Hemant Achhami <hemantachhami19@gmail.com>
2018-11-11 11:06:55 +0545

    Simple todo app completed with nodejs and mongodb

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -7,102 +7,112 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     console.log("Connecting with the mongo server");
     const db = client.db('TodoApp');
 
+
     /**********
      * CREATE
      **********/
-    //  // Insert one
-    // db.collection('TodoApp').insertOne({
-    //     text: 'test text1',
-    //     completed: false,
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log("error");
-    //     }
-    //     console.log(JSON.stringify(result.ops));
-    // });
-    //
-    // //InsertMany object
-    // todoObjectList = [
-    //     {
-    //         text:'Read a book',
-    //         completed:true
-    //     },
-    //     {
-    //         text:'complete node js crud',
-    //         completed:false
-    //     }
-    // ];
-    // db.collection("todos").insertMany(todoObjectList,(error,result)=>{
-    //     if(error) throw  error;
-    //     console.log(result.ops);
-    // });
 
-    /**********
-     * READ
-     **********/
+
+    // Insert one item in todos collection
+    db.collection('todos').insertOne({
+        text: 'do homework',
+        completed: false,
+    }, (error, result) => {
+        if (error) {
+            return console.log("error");
+        }
+        console.log(JSON.stringify(result.ops));
+    });
+
+    //InsertMany object
+    todoObjectList = [
+        {
+            text:'Read a book',
+            completed:true
+        },
+        {
+            text:'complete node js crud',
+            completed:false
+        }
+    ];
+    db.collection("todos").insertMany(todoObjectList,(error,result)=>{
+        if(error) throw  error;
+        console.log(result.ops);
+    });
+
+
+    /**************
+     * READ / FETCH
+     **************/
+
+
     //Fetch all items:
-    // db.collection('todos').find({}).toArray((error,result)=>{
-    //     if (error) throw  error;
-    //     console.log(result);
-    // });
+    db.collection('todos').find({}).toArray((error,result)=>{
+        if (error) throw  error;
+        console.log(result);
+    });
 
     //Fetch all item with some attributes text only
     //But results comes with id as well
     //To shut the _id we need to set the _id to 0 value
     //We cannot use both the 0 and 1 value at the same time except for the id value
-    // db.collection('todos').find({},{projection:{_id:0,text:1}}).toArray((error,result)=>{
-    //     if(error) throw error;
-    //     console.log(result);
-    // });
+    db.collection('todos').find({},{projection:{_id:0,text:1}}).toArray((error,result)=>{
+        if(error) throw error;
+        console.log(result);
+    });
 
     //Fetching one item with matching certain criteria
-    // var query = {text:'complete node js crud'};
-    // db.collection('todos').find(query).toArray((error,result)=>{
-    //    if(error){
-    //        throw  error;
-    //    }
-    //    console.log(result);
-    // });
+    var query = {text:'complete node js crud'};
+    db.collection('todos').find(query).toArray((error,result)=>{
+       if(error){
+           throw  error;
+       }
+       console.log(result);
+    });
 
     //Fetching one item with certain id
-    // db.collection('todos').findOne({_id :new ObjectID("5be7a524149d7c0953176c0a")},(error,result)=>{
-    //    if(error){
-    //        throw  error;
-    //    }
-    //    console.log(result);
-    // });
+    db.collection('todos').findOne({_id :new ObjectID("5be7a524149d7c0953176c0a")},(error,result)=>{
+       if(error){
+           throw  error;
+       }
+       console.log(result);
+    });
 
     /**********
-    *UPDATE*
-    **********/
+     *UPDATE*
+     **********/
+
+
     // updating a first object
-    // let myObject = { text:'Read a book1'};
-    // let newObject = {$set:{text:'Read a book2'}};
-    // db.collection('todos').updateOne(myObject,newObject,(error,result)=>{
-    //     if(error)
-    //         throw error;
-    //     console.log(result);
-    // });
+    var myObject = { text:'Read a book'};
+    var newObject = {$set:{text:'Read a book again'}};
+    db.collection('todos').updateOne(myObject,newObject,(error,result)=>{
+        if(error)
+            throw error;
+        console.log(result);
+    });
 
     //updating many objects in collections
-    // let myObject = {completed:true};
-    // let newObject = {$set:{completed:false}};
-    // db.collection('todos').updateMany(myObject,newObject,(error,result)=>{
-    //     if(error)
-    //         throw error;
-    //     console.log(result.result.nModified + " items modified");
-    // });
+    var myObject = {completed:true};
+    var newObject = {$set:{completed:false}};
+    db.collection('todos').updateMany(myObject,newObject,(error,result)=>{
+        if(error)
+            throw error;
+        console.log(result.result.nModified + " items modified");
+    });
 
     /**********
      *DELETE*
      **********/
+
+
     //Deleting one item which is completed
-    db.collection('todos').deleteOne({completed:true}).then((result)=>{
-       console.log(result);
+    db.collection('todos').deleteOne({completed: true}).then((result) => {
+        console.log(result);
     });
 
     //Deleting many items which are completed
-    db.collection('todos').deleteMany({completed:true}).then((result)=>{
+    db.collection('todos').deleteMany({completed: true}).then((result) => {
         console.log(result);
     });
 
